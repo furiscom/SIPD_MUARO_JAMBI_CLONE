40b02c6fca16ded062a11beb64a800878def8833
MUHAMMAD FURQAN HAKIM, S.Kom, MTA <138193168+furiscom@users.noreply.github.com>
2024-07-12 08:35:46 +0000

    index 1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD Application</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="mt-5">CRUD Application</h1>
+        <div class="list-group mt-3">
+            <a href="urusan.php" class="list-group-item list-group-item-action">Urusan</a>
+            <a href="bidang_urusan.php" class="list-group-item list-group-item-action">Bidang Urusan</a>
+            <a href="program.php" class="list-group-item list-group-item-action">Program</a>
+            <a href="kegiatan.php" class="list-group-item list-group-item-action">Kegiatan</a>
+            <a href="sub_kegiatan.php" class="list-group-item list-group-item-action">Sub Kegiatan</a>
+        </div>
+    </div>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
